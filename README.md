bc461a9215871db00d1be67f47c56ac5c2a65dc9
Aman Singh <amansingh08087@gmail.com>
2026-02-13 01:31:35 +0530

    initial_commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# capcut_clone
+
+Capcut Clone
+
+# Screenshots
---
+++ android/app/src/main/kotlin/com/ghostcorporation/capcut_clone/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ghostcorporation.capcut_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
