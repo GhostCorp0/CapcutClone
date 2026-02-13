53e536858e0e023950117426b88d71d84e7a5abe
Aman Singh <amansingh08087@gmail.com>
2026-02-13 18:00:16 +0530

    trim_slider

--- lib/features/editor/presentation/video_editor_screen.dart
+++ lib/features/editor/presentation/video_editor_screen.dart
@@ -1,9 +1,13 @@
 import 'dart:io';
 
+import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter/return_code.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:video_editor/video_editor.dart';
 import 'package:video_player/video_player.dart';
+import 'package:path/path.dart' as path;
 
 class VideoEditorScreen extends StatefulWidget {
   const VideoEditorScreen({super.key});
@@ -53,10 +57,42 @@ class _VideoEditorScreenState extends State<VideoEditorScreen> {
     }
   }
 
+  Future<void> _trimVideo() async {
+    if (_videoEditorController == null) return;
+    final start = _videoEditorController!.startTrim.inMilliseconds / 1000;
+    final end = _videoEditorController!.endTrim.inMilliseconds / 1000;
+    final Directory tempDir = await getTemporaryDirectory();
+    final String timestamp = DateTime.now().millisecondsSinceEpoch.toString();
+    final String fileName = 'trimmed_video_$timestamp.mp4';
+    print("filename : $fileName");
+    final String outputPath = path.join(tempDir.path, fileName);
+
+    final String command =
+        '-i ${_videoEditorController!.file.path} -ss $start -to $end -c copy $outputPath';
+    await FFmpegKit.execute(command).then((session) async {
+      final returnCode = await session.getReturnCode();
+      print('Returncode : $returnCode');
+      if (ReturnCode.isSuccess(returnCode)) {
+        setState(() {
+          trimmedVideos.add(outputPath);
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Video exported to: $outputPath")),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Failed to exported to : $outputPath")),
+        );
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("Video editor"),
+      ),
       body: Container(
         width: double.infinity,
         height: double.infinity,
@@ -133,6 +169,90 @@ class _VideoEditorScreenState extends State<VideoEditorScreen> {
                       },
                     ),
                   ],
+                ),
+              ),
+              //Timeline editor
+              Expanded(
+                flex: 2,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: trimmedVideos.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      width: 100,
+                      margin: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Stack(
+                        children: [
+                          Image.asset(
+                            'assets/images/capcut_logo.png',
+                            fit: BoxFit.cover,
+                          ),
+                          Positioned(
+                            child: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  trimmedVideos.removeAt(index);
+                                });
+                              },
+                              icon: Icon(Icons.delete, color: Colors.red),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+              //Trim Slider
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: TrimSlider(
+                  controller: _videoEditorController!,
+                  height: 60,
+                  child: TrimTimeline(controller: _videoEditorController!),
+                ),
+              ),
+
+              //Editing toolbar
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      onPressed: _trimVideo,
+                      icon: Icon(Icons.content_cut, color: Colors.white),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.crop, color: Colors.white),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.speed, color: Colors.white),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.music_note, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+              //Bottom action buttons
+              Padding(
+                padding: EdgeInsets.only(bottom: 25),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                  ),
+                  child: Text(
+                    'Merge & Export Final video',
+                    style: TextStyle(fontSize: 16),
+                  ),
                 ),
               ),
             ] else ...[
